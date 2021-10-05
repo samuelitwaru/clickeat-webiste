2a8440d73885667d3299c8984e12fc82aca7688e
samuelitwaru <samuelitwaru@gmail.com>
2021-10-05 12:39:29 +0300

    more

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,19 @@
 function sendRequest( method, url, data ){
 	
 }
+
+function loadProductImage(){
+	$('.product-image').each((index, each)=>{
+		if (each.complete) {
+			$(each).attr('hidden', false)
+			$(each.dataset.loader).hide()
+		}
+	})
+
+	$('.product-image').on("load", (event)=>{
+		$(event.target).attr('hidden', false)
+		$(event.target.dataset.loader).hide()
+	})
+}
+
+loadProductImage()
